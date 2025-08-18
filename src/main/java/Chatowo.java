ed31889d01aebf254de331381c0ade832fd87edf
Tan Yun Qi <tan_yun_qi@hotmail.sg>
2025-08-18 22:07:49 +0800

    Add support for echo

--- src/main/java/Chatowo.java
+++ src/main/java/Chatowo.java
@@ -1,11 +1,21 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Chatowo {
     public static void main(String[] args) {
-        String divider = "\n_____________";
+        String divider = "\n~*~*~*~*~*~*~*~*~*~*~*~*~*~";
 
-        String greeting = "Hewwo! I'm Chatowo. :3\nWhat can I do for you? OwO";
+        String greeting = "    Hewwo! I'm Chatowo. :3\n    What can I do for you? OwO";
         System.out.println(greeting + divider);
 
-        String exit = "Byebye -w-";
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        while (!Objects.equals(input, "bye")) {
+            System.out.println("    " + input + divider);
+            input = scanner.nextLine();
+        }
+
+        String exit = "    Byebye -w-";
         System.out.println(exit + divider);
     }
 }
