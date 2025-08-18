c3c7b0944f388774fdc160fc365d1e56b7b29e94
Tan Yun Qi <tan_yun_qi@hotmail.sg>
2025-08-18 22:24:22 +0800

    Add support for list

--- src/main/java/Chatowo.java
+++ src/main/java/Chatowo.java
@@ -8,10 +8,23 @@ public class Chatowo {
         String greeting = "    Hewwo! I'm Chatowo. :3\n    What can I do for you? OwO";
         System.out.println(greeting + divider);
 
+        String[] list = new String[100];
+        int count = 0;
+
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         while (!Objects.equals(input, "bye")) {
-            System.out.println("    " + input + divider);
+            if (Objects.equals(input, "list")) {
+                for (int i = 0; i < count; i++) {
+                    String index = String.valueOf(i + 1);
+                    System.out.println("    " + index + ". " + list[i]);
+                }
+            } else {
+                list[count] = input;
+                count++;
+                System.out.println("    added: " + input + divider);
+            }
+
             input = scanner.nextLine();
         }
 
