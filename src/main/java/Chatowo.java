7ebfbbbe3cf93fd59e35125d375e45a079f28964
Tan Yun Qi <tan_yun_qi@hotmail.sg>
2025-08-19 15:48:05 +0800

    Add support for todo, deadline and event

--- src/main/java/Chatowo.java
+++ src/main/java/Chatowo.java
@@ -1,16 +1,26 @@
 import java.util.Scanner;
 
 public class Chatowo {
-    public static void main(String[] args) {
-        String divider = "\n~*~*~*~*~*~*~*~*~*~*~*~*~*~";
+    // initialise task list
+    private static Task[] list = new Task[100];
+    private static int count = 0;
 
+    public static void reply(String msg) {
+        String divider = "\n~*~*~*~*~*~*~*~*~*~*~*~*~*~";
+        System.out.println(msg + divider);
+    }
+
+    public static void replyAddTask(Task task) {
+        list[count] = task;
+        count++;
+        Chatowo.reply("    Added " + list[count - 1] + " to task list! UwU\n" +
+                "    You have " + count + " tasks now!!");
+    }
+
+    public static void main(String[] args) {
         // output greeting
         String greeting = "    Hewwo! I'm Chatowo. :3\n    What can I do for you? OwO";
-        System.out.println(greeting + divider);
-
-        // initialise task list
-        Task[] list = new Task[100];
-        int count = 0;
+        Chatowo.reply(greeting);
 
         // initialise scanner for input
         Scanner scanner = new Scanner(System.in);
@@ -19,27 +29,52 @@ public class Chatowo {
         // continuously check input
         while (!input.equals("bye")) {
             if (input.equals("list")) {
+                String listtext = "    Here are your tasks! >w<";
                 for (int i = 0; i < count; i++) {
                     String index = String.valueOf(i + 1);
-                    System.out.println("    " + index + "." + list[i].toString());
+                    listtext += ("\n    " + index + "." + list[i].toString());
                 }
-                System.out.println(divider);
+                Chatowo.reply(listtext);
             } else {
                 String[] words = input.split(" ");
-                if (words[0].equals("mark")) {
-                    int index = Integer.parseInt(words[1]) - 1;
-                    list[index].setDone();
-                    System.out.println("    Okie! This task is done! ^w^\n      "
-                            + list[index].toString() + divider);
-                } else if (words[0].equals("unmark")) {
-                    int index = Integer.parseInt(words[1]) - 1;
-                    list[index].setNotDone();
-                    System.out.println("    Oh... This task is not done yet... OwO\n      "
-                            + list[index].toString() + divider);
-                } else {
-                    list[count] = new Task(input);
-                    count++;
-                    System.out.println("    added: " + input + divider);
+
+                switch(words[0]) {
+                    case "mark": {
+                        int index = Integer.parseInt(words[1]) - 1;
+                        list[index].setDone();
+                        Chatowo.reply("    Okie! This task is done! ^w^\n      "
+                                + list[index].toString());
+                        break;
+                    }
+
+                    case "unmark": {
+                        int index = Integer.parseInt(words[1]) - 1;
+                        list[index].setNotDone();
+                        Chatowo.reply("    Oh... This task is not done yet... OwO\n      "
+                                + list[index].toString());
+                        break;
+                    }
+
+                    case "todo":
+                        Chatowo.replyAddTask(new ToDo(input.substring(5)));
+                        break;
+
+                    case "deadline":
+                        int deadlineindex = input.lastIndexOf(" /by ");
+                        Chatowo.replyAddTask(new Deadline(input.substring(9, deadlineindex),
+                                input.substring(deadlineindex + 5)));
+                        break;
+
+                    case "event":
+                        int fromindex = input.lastIndexOf(" /from ");
+                        int toindex = input.lastIndexOf(" /to ");
+                        Chatowo.replyAddTask(new Event(input.substring(6, fromindex),
+                                input.substring(fromindex + 7, toindex),
+                                input.substring(toindex + 5)));
+                        break;
+
+                    default:
+                        Chatowo.replyAddTask(new Task(input));
                 }
             }
 
@@ -47,6 +82,6 @@ public class Chatowo {
         }
 
         String exit = "    Byebye -w-";
-        System.out.println(exit + divider);
+        Chatowo.reply(exit);
     }
 }
