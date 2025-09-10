7e0e8c16399ab1a919a61dcbf1e31604160eecac
Tan Yun Qi <tan_yun_qi@hotmail.sg>
2025-09-10 17:47:37 +0800

    Add basic undo function

--- src/main/java/chatowo/Chatowo.java
+++ src/main/java/chatowo/Chatowo.java
@@ -14,6 +14,9 @@ public class Chatowo {
     private TaskList list = new TaskList();
     private final Storage storage = new Storage("./data/chatowo.txt");
     private final Parser parser = new Parser(this);
+    private String prevCommand = null;
+    private Task prevCommandTask = null;
+    private int prevCommandIndex = -1;
 
     public String getResponse(String input) {
         return parser.parse(input);
@@ -37,6 +40,11 @@ public class Chatowo {
             list.add(task);
             storage.addTask(task);
             assert list.contains(task);
+
+            this.prevCommand = "add";
+            this.prevCommandTask = task;
+            this.prevCommandIndex = list.size() - 1;
+
             return "Added " + task + " to task list! UwU\n"
                     + "You have " + list.size() + " tasks now!!";
         } catch (Exception e) {
@@ -60,6 +68,10 @@ public class Chatowo {
             return e.getMessage();
         }
 
+        this.prevCommand = "mark";
+        this.prevCommandTask = t;
+        this.prevCommandIndex = index;
+
         return "Okie! This task is done! ^w^\n  " + t;
     }
 
@@ -79,6 +91,10 @@ public class Chatowo {
             return e.getMessage();
         }
 
+        this.prevCommand = "unmark";
+        this.prevCommandTask = t;
+        this.prevCommandIndex = index;
+
         return "Oh... This task is not done yet... OwO\n  " + t;
     }
 
@@ -93,6 +109,11 @@ public class Chatowo {
 
         try {
             storage.removeFromTaskList(index);
+
+            this.prevCommand = "delete";
+            this.prevCommandTask = t;
+            this.prevCommandIndex = index;
+
             return "Okie! This task has been deleted! ^w^\n  " + t;
         } catch (IOException e) {
             return e.getMessage();
@@ -106,6 +127,29 @@ public class Chatowo {
      */
     public String find(String phrase) {
         return "Here are the matching tasks! >w<" + list.find(phrase);
+    }
+
+    /**
+     * Undo the command that was last executed.
+     */
+    public String undo() {
+        if (this.prevCommand == null) {
+            return "I forgor what was the last command you did... Sorry >m<";
+        }
+
+        switch (this.prevCommand) {
+        case "mark":
+            return this.unmark(this.prevCommandIndex);
+        case "unmark":
+            return this.mark(this.prevCommandIndex);
+        case "delete":
+            return this.addTask(this.prevCommandTask);
+        case "add":
+            return this.delete(this.prevCommandIndex);
+        default:
+            assert false;
+            return "";
+        }
     }
 
     /**
