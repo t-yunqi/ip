60847624adfafc5a12ed5357af2b3bbcff760d72
Tan Yun Qi <tan_yun_qi@hotmail.sg>
2025-09-17 23:47:12 +0800

    Add bye delay

--- src/main/java/chatowo/ui/MainWindow.java
+++ src/main/java/chatowo/ui/MainWindow.java
@@ -1,6 +1,7 @@
 package chatowo.ui;
 
 import chatowo.Chatowo;
+import javafx.animation.PauseTransition;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -9,6 +10,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.util.Duration;
 
 /**
  * Controller for the main GUI.
@@ -59,7 +61,9 @@ public class MainWindow extends AnchorPane {
         userInput.clear();
 
         if (input.equals("bye")) {
-            Platform.exit();
+            PauseTransition delay = new PauseTransition(Duration.seconds(1));
+            delay.setOnFinished(event -> Platform.exit());
+            delay.play();
         }
     }
 }
